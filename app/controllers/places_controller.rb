04ea2ff926d3cb5cc50048d99a959b44a53096e3
karlo malecki <karlo.malecki@gmail.com>
2023-03-06 19:52:43 -0600

    crud actions for the #Places but missing the views, will finish

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,30 @@
 class PlacesController < ApplicationController
   def index
+    @places = Place.all()
   end
+
+  def show
+    @places = Place.find(params[:id])
+  end
+
+  def edit
+    @places = Place.find(params[:id])
+  end
+
+  def new
+    @places = Place.new
+  end
+
+  def create
+   place = Place.create(place_params)
+
+   redirect_to_places_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :address)
+  end
+
 end
