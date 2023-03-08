ef21604624bc5d6a1efc0334400987cc9dd14d15
karlo malecki <karlo.malecki@gmail.com>
2023-03-08 17:50:27 -0600

    geocoding is fuckin done :) :)

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,8 @@ class Place < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
   has_many :reviews
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   include PgSearch::Model
 
