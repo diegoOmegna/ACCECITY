498d66a37c548da127fe02c40618b4501a85eb6c
diego andres omegna <domegna@gmail.com>
2023-03-12 22:09:34 +0100

    still things to do

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,17 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @places = Place.top.first(5)
+
+    @places.each do |place|
+      @reviews = Review.where(place_id: place.id).order("created_at DESC")
+      if @reviews.blank?
+        @avg_review = 0
+      else
+        @avg_review = @reviews.average(:rating).round(2)
+      end
+    end
+
     # @places = Place.search_by_name_and_details(params[:query])
     # @user = current_user
   end
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import FlickityController from "./flickity_controller"
+application.register("flickity", FlickityController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
